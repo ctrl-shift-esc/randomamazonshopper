6bfec9e0d5e8e2803386e0f7c4766fdf01e95d5b
Krishna Sudhakar <virus.krishna89@gmail.com>
2013-08-02 19:41:19 +0200

    Updated login credentials

--- myscript.js
+++ myscript.js
@@ -10,10 +10,10 @@ casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
 //opening amazon login page
 casper.start('https://www.amazon.de/ap/signin?_encoding=UTF8&openid.assoc_handle=deflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.de%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dgno_signin');
 
-//performing login
+//performing login. Mind that the email & password doesn't work ;)
 casper.thenEvaluate(function(){
-	document.querySelector('input[name="email"]').setAttribute('value', "amazonshopperbot@gmail.com");
-	document.querySelector('input[name="password"]').setAttribute('value', "amazonshopperbot");
+	document.querySelector('input[name="email"]').setAttribute('value', "shopperbot@gmail.com");
+	document.querySelector('input[name="password"]').setAttribute('value', "password_here");
 	document.querySelector('form[name="signIn"]').submit();
 }, 'CasperJS');
 
