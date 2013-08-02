60f365a926a83b9178dcc4d96cd4dc66171e3d8e
Krishna Sudhakar <virus.krishna89@gmail.com>
2013-08-02 19:07:13 +0200

    Uploading the javascript of the bot

---
+++ myscript.js
@@ -0,0 +1,46 @@
+var casper = require('casper').create({
+	viewportSize: {
+        width: 1024,
+        height: 768
+    }
+});
+//setting cookies
+casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
+
+//opening amazon login page
+casper.start('https://www.amazon.de/ap/signin?_encoding=UTF8&openid.assoc_handle=deflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.de%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dgno_signin');
+
+//performing login
+casper.thenEvaluate(function(){
+	document.querySelector('input[name="email"]').setAttribute('value', "amazonshopperbot@gmail.com");
+	document.querySelector('input[name="password"]').setAttribute('value', "amazonshopperbot");
+	document.querySelector('form[name="signIn"]').submit();
+}, 'CasperJS');
+
+//getting random word
+
+
+//Performing search
+casper.thenEvaluate(function(){
+	document.querySelector('input[name="field-keywords"]').setAttribute('value', "keyboard");
+	document.querySelector('form[name="site-search"]').submit();
+}, 'CasperJS');
+
+
+//taking screenshot - used for debug
+casper.then(function(){
+	this.viewport(1280, 1024);
+	this.wait(5000);
+
+});
+
+casper.then(function(){
+	this.capture('amazon.png',{
+		top: 0,
+		left: 0,
+		width: 1280,
+		height: 1024
+	});
+});
+
+casper.run();
